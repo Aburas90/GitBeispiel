14f733a77ee8c85d84afbd9c08fad1ff0cfd481c
unknown <aburas90@msn.com>
2023-05-18 21:55:43 +0200

    Summe wurde implementiert

--- Main.java
+++ Main.java
@@ -8,4 +8,13 @@
 
 public class Main
 {
+    public static void main (String args[])
+    {
+        int a = 10;
+        int sum = 0;
+        for(int i=0; i<=a; ++i)
+        {
+            sum += i;
+        }
+    }
 }
