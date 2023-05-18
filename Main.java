97b23b461e0297e35b98c6554991407e50ba328f
unknown <aburas90@msn.com>
2023-05-18 22:18:45 +0200

    Multiplikation wurde umgesetzt

--- Main.java
+++ Main.java
@@ -16,5 +16,11 @@ public class Main
         {
             sum += i;
         }
+        int b = 5;
+        int mult = 1;
+        for (int i=2; i<=b; ++i)
+        {
+            mult *= i;
+        }
     }
 }
