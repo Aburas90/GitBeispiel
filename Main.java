88eeab27b08866b0ae2bc654db4a6abf9aee2852
unknown <aburas90@msn.com>
2023-05-18 22:58:12 +0200

    Variable ob mit dem Wert 10 für obere Grenze

--- Main.java
+++ Main.java
@@ -10,15 +10,15 @@ public class Main
 {
     public static void main (String args[])
     {
-        int a = 10;
+        int ob = 10;
         int sum = 0;
-        for(int i=0; i<=a; ++i)
+        for(int i=0; i<=ob; ++i)
         {
             sum += i;
         }
         
         int mult = 1;
-        for (int i=2; i<=a; ++i)
+        for (int i=2; i<=ob; ++i)
         {
             mult *= i;
         }
